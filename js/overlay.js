45a25638742f4ccd12c052b3b50f2064fce76444
jsj-c <745864360@qq.com>
2016-10-20 18:21:09 +0800

    modified:   js/overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -21,28 +21,40 @@ function init() {
   renderer.setSize(w, h);
   renderer.setClearColor(0x000000);
   renderer.autoClear = false;
+  renderer.shadowMapEnabled = true;
   document.body.appendChild(renderer.domElement);
 
   perScene = new THREE.Scene();
-  perCamera = new THREE.PerspectiveCamera(60, w/h, 0.1, 1000);
+  perCamera = new THREE.PerspectiveCamera(60, w/h, 0.1, 10000);
   perCamera.position.set(350, 350, 350);
   perCamera.lookAt(perScene.position);
 
   light = new THREE.DirectionalLight();
-  light.position.set(100, 500, 200);
+  light.position.set(700, 400, -500);
   light.castShadow = true;
+  //light.lookAt(perScene.position);
+  light.shadowCameraNear = 250;
+  light.shadowCameraFar = 2000;
+  light.shadowCameraLeft = -500;
+  light.shadowCameraRight = 500;
+  light.shadowCameraTop = 500;
+  light.shadowCameraBottom = -500;
+  light.shadowMapWidth = 2048;
+  light.shadowMapHeight = 2048;
+  light.shadowCameraVisible = true;
+  light.name ='dlight';
   perScene.add(light);
   perScene.add(new THREE.AxisHelper(500));
 
   var sphere = new THREE.SphereGeometry(30, 10, 10);
   var sphereMesh = new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
-    map: loader.load('img/t3.jpg'),
-    bumpScale: 2
+    //map: loader.load('img/t3.jpg'),
+    refractionRatio: 0.98,
   }))  
-  sphereMesh.material.map.wrapT = sphereMesh.material.map.wrapS = THREE.RepeatWrapping;
-  sphereMesh.material.map.repeat.set(4,4);
   sphereMesh.name = 'sphere';
-
+  sphereMesh.position.set(100, 30, 0);
+  sphereMesh.material.refractionRatio = 0.98;
+  sphereMesh.castShadow = true;
   perScene.add(sphereMesh);
 
   /*2d元素*/
@@ -55,6 +67,8 @@ function init() {
   //videoTexture();
   /*多种材质*/
   multiMaterial();
+  /*反射*/
+  //cubeMap();
   initControls();
   render();
   
@@ -90,21 +104,61 @@ function multiMaterial() {
   //var mesh = new THREE.Mesh(g, new THREE.MultiMaterial([materialL, materialP]));
   mesh.position.set(-50, 50, 0);
   perScene.add(mesh);
+
+  var s = new THREE.SphereGeometry(50, 10, 10);
+  var materials = [];
+  var count = 0;
+  s.faces.forEach(function(f) {
+    f.materialIndex = count++;
+    var m = new THREE.MeshBasicMaterial({
+      color: Math.random() * 0xffffff,
+    });
+    if (count % 2 == 0) {
+      m.transparent = true;
+      m.opacity = 0.4;
+    }
+    m.side = THREE.DoubleSide;
+    materials.push(m);
+  })
+  var sm = new THREE.Mesh(s, new THREE.MultiMaterial(materials));
+  sm.position.set(-100, 100, 100);
+  sm.name = 'separateMaterialMesh';
+  perScene.add(sm);
+}
+
+function cubeMap() {
+  var path = 'earth';
+  var urls = [
+    'img/' + path + '/px.jpg',
+    'img/' + path + '/nx.jpg',
+    'img/' + path + '/py.jpg',
+    'img/' + path + '/ny.jpg',
+    'img/' + path + '/pz.jpg',
+    'img/' + path + '/nz.jpg'
+  ];
+  var cubeLoader = new THREE.CubeTextureLoader();
+  var cubeMap = new THREE.CubeTextureLoader().load(urls);
+
+  perScene.getObjectByName('sphere').material.envMap = cubeMap;
+
 }
 
 function canvasTexture() {
   var canvas = document.createElement('canvas');
+  clock(canvas);
   canvas.width = 512
   canvas.height = 512;
+  
 
-  clock(canvas);
-
-  var canvasMesh = new THREE.Mesh(new THREE.BoxGeometry(40, 40, 40),
-    new THREE.MeshLambertMaterial({
+  var canvasMesh = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100),
+    new THREE.MeshPhongMaterial({
       map: new THREE.Texture(canvas),
+      //specular: 0xfffff,
     })
   );
-  canvasMesh.position.y = 40;
+  canvasMesh.castShadow = true;
+  canvasMesh.position.set(-100, 100, -100);
+  canvasMesh.rotation.y = - Math.PI / 4;
   canvasMesh.name = 'canvasMesh';
   perScene.add(canvasMesh);
 }
@@ -134,9 +188,15 @@ function include(path) {
 }
 
 function addFloor() {
-  var floorGeometry = new THREE.PlaneGeometry(500, 500, 20, 20);
+  var slight = new THREE.SpotLight();
+  slight.position.set(0, 500, 0);
+  //perScene.add(slight);
+
+  var floorGeometry = new THREE.PlaneGeometry(800, 800, 40, 40);
   var floorMaterial = new THREE.MeshPhongMaterial({
-    bumpScale: 0
+    //bumpScale: 0,
+    //transparent: true,
+    //opacity: 1,
   });
   /*normalMap 法向贴图*/
   floorMaterial.map = loader.load('img/t1.jpg');
@@ -144,6 +204,7 @@ function addFloor() {
   floorMaterial.map.repeat.set(8, 8);
 
   var floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+  floorMesh.receiveShadow = true;
   floorMesh.receiveShadow = true;
   floorMesh.rotation.x = -Math.PI/2;
   perScene.add(floorMesh);
@@ -154,18 +215,29 @@ function initControls() {
 }
 
 var theta = 0;
+var gama = 0;
+var rv = 0.04;
 function render() {
   var s = perScene.getObjectByName('sphere');
-  theta += 0.01;
+  var dl = perScene.getObjectByName('dlight');
+  theta += rv;
+  gama += 0.02
 
   s.position.x = 100 * Math.cos(theta);
-  s.position.y = 100 * Math.sin(theta);
+  s.position.y = 25 + 50 * Math.sin(theta);
+
+  dl.position.x = 700 * Math.cos(gama);
+  dl.position.z = 700 * Math.sin(gama);
+  if (theta > Math.PI || theta < 0) {
+    rv = -rv;
+  }
 
   perScene.getObjectByName('canvasMesh').material.map.needsUpdate = true;
   //perScene.getObjectByName('videoMesh').material.map.needsUpdate = true;
+  perScene.getObjectByName('separateMaterialMesh').rotation.x +=0.01;
 
   //perCamera.lookAt(s.position);
-  renderer.clear();
+  //renderer.clear();
   renderer.render(perScene, perCamera);
   //renderer.clearDepth();
   //renderer.render(orthoScene, orthoCamera);
